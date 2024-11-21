a840dd874b8ffb5be8887c77a03ee420f53e8255
Christian-Bohorquez <mau162002@hotmail.com>
2024-11-20 23:53:44 -0500

    initial Commit - Pantalla de Bienvenida

---
+++ app/src/main/java/com/prueba/proyecto_dam_p1/inicio/FirstActivity.kt
@@ -0,0 +1,16 @@
+package com.prueba.proyecto_dam_p1.inicio
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.prueba.proyecto_dam_p1.R
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+
+    }
+}
